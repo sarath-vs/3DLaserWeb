a3fd38d0399795434cfbcff5b6a4605dc379a863
sarath sathyabalan <66293762+sarath-vs@users.noreply.github.com>
2023-07-08 21:34:03 +0530

    url

--- lib/domain/remote/url/url_pool.dart
+++ lib/domain/remote/url/url_pool.dart
@@ -9,7 +9,7 @@ class URLPool {
 // test
 //http://65.1.86.132
 //local czech
-  final _baseUrl = 'http://qualitynewapi.lasertec.eu/accounts';
+  final _baseUrl = 'http://qualitynew.lasertec.eu/backend/accounts';
 
   final _login = '/login/';
   final _refresh = '/refreshtoken/';
@@ -19,50 +19,50 @@ class URLPool {
 
   String get login => _baseUrl + _login;
   String get refreshTocken => _baseUrl + _refresh;
-  // String get getProductname => 'http://qualitynewapi.lasertec.eu/admin-dashboard/categories/';
-  // String get getProductType => 'http://qualitynewapi.lasertec.eu/products/sub-categories/';
-  // String get getInitQstnModules => 'http://qualitynewapi.lasertec.eu/questions/modules/';
-  // String get getQuestions => 'http://qualitynewapi.lasertec.eu/questions/';
-  // String get saveAnswer => 'http://qualitynewapi.lasertec.eu/questions/answer/';
-  // String get getDigitalMannualList => 'http://qualitynewapi.lasertec.eu/digital-manual/';
-  // String get getDigitalMannualDetail => 'http://qualitynewapi.lasertec.eu/digital-manual/';
+  // String get getProductname => 'http://qualitynew.lasertec.eu/backend/admin-dashboard/categories/';
+  // String get getProductType => 'http://qualitynew.lasertec.eu/backend/products/sub-categories/';
+  // String get getInitQstnModules => 'http://qualitynew.lasertec.eu/backend/questions/modules/';
+  // String get getQuestions => 'http://qualitynew.lasertec.eu/backend/questions/';
+  // String get saveAnswer => 'http://qualitynew.lasertec.eu/backend/questions/answer/';
+  // String get getDigitalMannualList => 'http://qualitynew.lasertec.eu/backend/digital-manual/';
+  // String get getDigitalMannualDetail => 'http://qualitynew.lasertec.eu/backend/digital-manual/';
   String get getToolsList =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/tools/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/tools/';
   String get saveTools =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/tools/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/tools/';
   String get deleteTools =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/tools';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/tools';
 
   String get getQualityProducts =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/category/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/category/';
   String get saveQualityProducts =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/category/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/category/';
   String get deleteQualityProducts =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/category';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/category';
   String get getQualityQuestions =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/question/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/question/';
   String get deleteQualityQuestions =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/question/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/question/';
   // String get editQualityQuestions =>
-  //     'http://qualitynewapi.lasertec.eu/admin-dashboard/question/';
+  //     'http://qualitynew.lasertec.eu/backend/admin-dashboard/question/';
   String get postQualityQuestions =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/question/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/question/';
 
   String get editQualityQuestions =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/question/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/question/';
   String get getAnsweredProduct =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/answers/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/answers/';
 
   String get assemblyDetails =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/assembly/category/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/assembly/category/';
   String get assemplyCompletedList =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/assembly/products/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/assembly/products/';
   String get assemblyQuestionDetails =>
-      'http://qualitynewapi.lasertec.eu/admin-dashboard/assembly/question/';
+      'http://qualitynew.lasertec.eu/backend/admin-dashboard/assembly/question/';
   String get finalAssemblyProductList =>
-      'http://qualitynewapi.lasertec.eu/final-admin/category/';
+      'http://qualitynew.lasertec.eu/backend/final-admin/category/';
   String get finalAssembly =>
-      'http://qualitynewapi.lasertec.eu/final-admin/category/';
+      'http://qualitynew.lasertec.eu/backend/final-admin/category/';
   String get finalAssemblyQuestion =>
-      'http://qualitynewapi.lasertec.eu/final-admin/question/';
+      'http://qualitynew.lasertec.eu/backend/final-admin/question/';
 }
